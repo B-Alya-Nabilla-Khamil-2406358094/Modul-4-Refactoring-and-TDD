8de5cef09352968b9b3335bce8275d109299f098
Alyanabilla <alyanabila486@gmail.com>
2026-03-10 15:34:17 +0700

    Implement enums in OrderRepository.javaepositorytest

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class OrderRepositoryTest {
+public class OrderRepositoryTest {
     OrderRepository orderRepository;
     List<Order> orders;
 
